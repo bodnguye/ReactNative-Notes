82cc0f985b6f99db782b03606d4f21cdf3341f97
One Punch <bodnguye@gmail.com>
2019-11-17 20:03:05 -0800

    multiple components

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,40 +1,29 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
+  
   const [courseGoals, setCourseGoals] = useState([]);
 
-const goalInputHandler = (enteredText) => {
-  setEnteredGoal(enteredText);
-};
 
-const addGoalHandler = () => {
+const addGoalHandler = goalTitle => {
   setCourseGoals(currentGoals => [
     ...currentGoals,          // guaranteed to get the latest snapshot of coure goal in react native
-    { key: Math.random().toString(), value: enteredGoal }
+    { id: Math.random().toString(), value: goalTitle }
   ]);  
 };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder='Course Goal'
-          style={styles.input} 
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-          />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
-      <FlatList           // FlatList expects an array of objects
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList 
+      keyExtractor={(item, index) => item.id} 
       data={courseGoals} 
-      renderItem={itemData => (
-        <View style={styles.listItem}>              
-            <Text>{itemData.item.value}</Text>     
-        </View>                                     // item has a key and value property
-        )}
-      />
+      renderItem={itemData => <GoalItem title={itemData.item.value} />}
+      />  
     </View>
   );
 }
@@ -43,21 +32,4 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  inputContainer: { 
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    alignItems: 'center' },
-  input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
-  }
 });
