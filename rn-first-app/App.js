1bb0e547981ac8714f8f8cef33418fdfe8facb2f
One Punch <bodnguye@gmail.com>
2019-11-17 17:16:32 -0800

    inputHandler and addGoalhandler

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -2,17 +2,30 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+const goalInputHandler = (enteredText) => {
+  setEnteredGoal(enteredText);
+};
+
+const addGoalHandler = () => {
+  setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);  // guaranteed to get the latest snapshot of coure goal in react native
+};
+
   return (
-    <View style={{padding: 50}}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput 
-          placeholder='Course Goal' 
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }} />
-        <Button title="ADD" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Course Goal'
+          style={styles.input} 
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+          />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View>
-
+        {courseGoals.map((goal) => <Text>{goal}</Text>)}
       </View>
 
     </View>
@@ -20,5 +33,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50
+  },
+  inputContainer: { 
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center' },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
