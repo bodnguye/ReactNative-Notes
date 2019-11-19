313cf3c37a3a88df5cccad7584dd60671743e669
One Punch <bodnguye@gmail.com>
2019-11-18 19:56:07 -0800

    modal slide

--- rn-first-app/components/GoalInput.js
+++ rn-first-app/components/GoalInput.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 
 
 
@@ -9,32 +9,35 @@ const GoalInput = props => {
 
     const goalInputHandler = enteredText => {
         setEnteredGoal(enteredText);
-      }; 
+    };
 
     return (
-         <View style={styles.inputContainer}>
-            <TextInput
-                placeholder='Course Goal'
-                style={styles.input} 
-                onChangeText={goalInputHandler}
-                value={enteredGoal}
-            />
-            <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
-        </View>
-    )
+        <Modal visible={props.visible} animationType='slide' >
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder='Course Goal'
+                    style={styles.input}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+                <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+            </View>
+        </Modal>
+    );
 };
 
-const styles= StyleSheet.create({
-    inputContainer: { 
-        flexDirection: 'row', 
-        justifyContent: 'space-between', 
-        alignItems: 'center' },
-      input: {
+const styles = StyleSheet.create({
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
         width: '80%',
         borderColor: 'black',
         borderWidth: 1,
         padding: 10
-      }
+    }
 });
 
 export default GoalInput;
